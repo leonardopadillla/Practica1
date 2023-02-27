3f2ae42d041973db843f0638863176d1a55d5211
leonardopadillla <84750145+leonardopadillla@users.noreply.github.com>
2023-02-26 22:18:40 -0600

    Initial commit

---
+++ Controllers/equiposController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Practica1.Models;
+
+
+
+namespace Practica1.Properties
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+
+    {
+        private readonly equiposContext _equiposContexto;
+        public equiposController(equiposContext equiposContexto)
+        {
+            _equiposContexto = equiposContexto;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipo = (from e in _equiposContexto.equipos
+                                           select e).ToList();
+
+            if (listadoEquipo.Count () == 0)
+            {
+                return NotFound();
+            }
+                 return Ok(listadoEquipo);
+
+        }
+    }
+}
